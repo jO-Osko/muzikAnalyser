35e50b46b82dde34c4b264288d01b927571d0b6a
Filip <koprivec.filip@gmail.com>
2016-09-11 21:54:40 +0200

    Added reading from file

--- models/Song.py
+++ models/Song.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Song model"""
+import csv
 import string
 from typing import List
 
@@ -32,7 +33,7 @@ class Canonizer:
     def canonize(self, name: str) -> str:
         name = self.translate(name.lower()).split()
         name = filter(lambda x: self.is_allowed_word(x), name)
-        return " ".join(name)
+        return " ".join(sorted(name))
 
 
 class Song(PrintableStructure):
@@ -42,16 +43,17 @@ class Song(PrintableStructure):
     __printable_name__ = "Song"
 
     cannonizer = Canonizer()
+    analyzing_servers = {514, 442, 443, 590, 1152, 520, 516, 1157, 470, 453, 451, 471}
 
     def __init__(self, muzik_id, name, server, bit_rate, duration, size, frequency):
-        self.muzik_id = muzik_id                        # type: str
-        self.name = name                                # type: str
-        self.server = int(server)                       # type: int
-        self.bit_rate = int(bit_rate)                   # type: int
-        self.duration = int(duration)                   # type: int
-        self.size = int(size)                           # type: int
-        self.frequency = int(frequency)                 # type: int
-        self.canonic_name = self.make_canonic_name()    # type: str
+        self.muzik_id = muzik_id                                    # type: str
+        self.name = name                                            # type: str
+        self.server = int(server)                                   # type: int
+        self.bit_rate = int(bit_rate) if bit_rate else 0            # type: int
+        self.duration = int(duration) if duration else 0            # type: int
+        self.size = int(size) if size else 0                        # type: int
+        self.frequency = int(frequency) if frequency else 0         # type: int
+        self.canonic_name = self.make_canonic_name()                # type: str
 
     # Canonize name for song name comparison
     def make_canonic_name(self):
@@ -60,14 +62,27 @@ class Song(PrintableStructure):
     def to_comparable_song(self) -> "ComparableSong":
         return ComparableSong(self)
 
-    @staticmethod
-    def list_all_songs() -> List["Song"]:
-        # Mock
-        return [Song("1", "harmonika", "muzik", 100, 100, 100, 100)]
+    def has_analyzing_server(self):
+        return self.server in self.analyzing_servers
 
     @staticmethod
-    def list_comparable_songs() -> List["ComparableSong"]:
-        return [song.to_comparable_song() for song in Song.list_all_songs()]
+    def list_all_songs(file_path="data/main.csv") -> List["ComparableSong"]:
+        songs = []
+        with open(file_path, encoding="utf8") as in_file:
+            reader = csv.reader(in_file)
+            for row in reader:
+                # Database is corrupted
+                try:
+                    song = Song.get_from_csv_array(row).to_comparable_song()
+                    if song.song.has_analyzing_server():
+                        songs.append(Song.get_from_csv_array(row).to_comparable_song())
+                except Exception as e:
+                    print(e)
+        return songs
+
+    @classmethod
+    def get_from_csv_array(cls, array):
+        return cls(array[0], array[2], array[1], array[12], array[10], array[11], array[15])
 
 
 class ComparableSong(PrintableStructure):
