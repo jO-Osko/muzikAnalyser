02add497ca0962b523d680874a9700ef140cf5a3
Filip <koprivec.filip@gmail.com>
2016-09-11 22:00:36 +0200

    Added main procedure to digest data

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 # -*- coding: utf-8 -*-
 
 """Muzik analyser"""
+from collections import defaultdict
+import pickle
+import os.path
+
+from models.Song import Song
 
 __author__ = "Filip Koprivec"
 
 
+def main():
+    songs = Song.list_all_songs()
+
+    print("All songs:", len(songs))
+
+    similar = defaultdict(list)
+
+    for song in songs:
+        similar[song.comparable_name].append(song)
+
+    duplicates = []
+    for name, duplicate_songs in similar.items():
+        if len(duplicate_songs) > 2:
+            duplicates.append(duplicate_songs)
+
+    pickle.dump(songs, open(os.path.join("data", "output", "songs.out"), "wb"))
+    pickle.dump(duplicates, open(os.path.join("data", "output", "duplicates.out"), "wb"))
+    print("Duplicates:", len(duplicates))
+
+
 if __name__ == '__main__':
-    pass
+    main()
