1fcc5647bf7a8a18d49679f9c175a481ca9562eb
Filip <koprivec.filip@gmail.com>
2016-09-11 23:08:39 +0200

    Creating different file for searching servers

--- utils/utils.py
+++ utils/utils.py
@@ -17,10 +17,19 @@ class Utils:
         base_conn = sqlite3.connect(os.path.join("data", "sqlite.db"))
         base_conn.text_factory = lambda x: str(x, "utf8", "ignore")
         cursor = base_conn.cursor()
-                                                                                        # I know this is bad, but normal parametrized queries do not work
-        cursor.execute("SELECT id, name, server, bitr, time, size, freq from main") #" where server in {servers}".format(servers=servers))
 
+        cursor.execute("SELECT id, name, server, bitr, time, size, freq FROM main")
         with open(os.path.join("data", "export.csv"), "w", encoding="utf8", newline="") as out_csv:
+            csv_writer = csv.writer(out_csv, quoting=csv.QUOTE_MINIMAL)
+            for result in cursor:
+                csv_writer.writerow(list(result))
+
+        # Export for validation
+        cursor.execute("SELECT id, name, server, bitr, time, size, freq FROM main "
+                        # I know this is bad, but normal parametrized queries do not work
+                       "WHERE server IN {servers}".format(servers=servers))
+
+        with open(os.path.join("data", "export_servers.csv"), "w", encoding="utf8", newline="") as out_csv:
             csv_writer = csv.writer(out_csv, quoting=csv.QUOTE_MINIMAL)
             for result in cursor:
                 csv_writer.writerow(list(result))
