8a808c50b0a7216204297b13f1c7edffe9e3a838
Filip <koprivec.filip@gmail.com>
2016-09-12 16:49:17 +0200

    Added new servers and expor parameter

--- utils/utils.py
+++ utils/utils.py
@@ -11,28 +11,29 @@ import os.path
 
 
 class Utils:
-
-    base_servers = {442: "balkanmp3_new", 443: "balkanmp3s_old", 451: "yucafe_old", 453: "yucafe_new",
-                        470: "narodnjak.si", 471: "zlatizvoki.com", 514: "balkandj", 516: "mixoteka", 520: "megamixers",
-                        590: "folkoteka", 1152: "klav.ma3x.org", 1157: "muzickinet"}
+    base_servers = {10: "sunka", 144: "svastaaws", 360: "kravampx", 442: "balkanmp3_new", 443: "balkanmp3s_old", 451: "yucafe_old",
+                    453: "yucafe_new", 470: "narodnjak.si", 471: "zlatizvoki.com", 514: "balkandj", 516: "mixoteka",
+                    520: "megamixers", 544: "kravampv", 568: "upload", 590: "folkoteka", 600: "exyudownload",
+                    1152: "klav.ma3x.org", 1155: "hrvatskihost", 1157: "muzickinet"}
 
     def __init__(self, servers=None):
         self.servers = servers if servers else self.base_servers
 
         self.server_ids = [j for j in self.servers]
 
-    def export_database(self):
+    def export_database(self, export_full=False):
         if not os.path.exists(os.path.join("data", "sqlite.db")):
             raise FileNotFoundError()
         base_conn = sqlite3.connect(os.path.join("data", "sqlite.db"))
         base_conn.text_factory = lambda x: str(x, "utf8", "ignore")
         cursor = base_conn.cursor()
 
-        cursor.execute("SELECT id, name, server, bitr, time, size, freq FROM main")
-        with open(os.path.join("data", "export.csv"), "w", encoding="utf8", newline="") as out_csv:
-            csv_writer = csv.writer(out_csv, quoting=csv.QUOTE_MINIMAL)
-            for result in cursor:
-                csv_writer.writerow(list(result))
+        if export_full:
+            cursor.execute("SELECT id, name, server, bitr, time, size, freq FROM main")
+            with open(os.path.join("data", "export.csv"), "w", encoding="utf8", newline="") as out_csv:
+                csv_writer = csv.writer(out_csv, quoting=csv.QUOTE_MINIMAL)
+                for result in cursor:
+                    csv_writer.writerow(list(result))
 
         # Export for validation
         cursor.execute("SELECT id, name, server, bitr, time, size, freq FROM main "
